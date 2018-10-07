74ecc5063a4b786e5eec1a3401cc8858180594d4
Aleksandr Sidorov <sidorovav@gmail.com>
2018-10-08 00:35:48 +0300

    до 7 примера

--- js/app.js
+++ js/app.js
@@ -50,4 +50,39 @@ var app4 = new Vue({
         { text: 'Создать что-нибудь классное' }
       ]
     }
+  });
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Привет, Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Привет, Vue!'
+    }
+});
+
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
   })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Овощи' },
+        { id: 1, text: 'Сыр' },
+        { id: 2, text: 'Что там ещё люди едят?' }
+      ]
+    }
+  });
