eaf15b85d6d5c0c7595473524be447a0d524c87b
Aleksandr Sidorov <sidorovav@gmail.com>
2018-10-07 22:48:59 +0300

    еще не все работет правильно

--- js/app.js
+++ js/app.js
@@ -5,13 +5,20 @@ new Vue({
         title: 'пример использования фреймворка Vue, кликните на элемент списка',
         pacients: pacients,
         pac:pacients[0],
-        selectedPacIndex:0
+        selectedPacIndex:0,
+        shearch:''
     },
     methods: { 
-        selectPacient: function (id){
-             console.log(pacients[id]);
-             this.pac = pacients[id];
-             this.selectedPacIndex=id;
+        selectPacient(pac,i){
+            this.selectPacient = i;
+            this.pac = pac;
+        }
+    },
+    computed:{ 
+        filtredPacients() {
+            return this.pacients.filter(pac => {
+                return pac.indexOf(this.shearch) > -1 
+            });
         }
     }
 });
